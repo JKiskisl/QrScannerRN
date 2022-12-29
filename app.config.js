ff42a90ee44b69a7b3353129b2335d905e766428
JKiskisl <smicscl@gmail.com>
2022-12-29 02:13:18 +0200

    logo

--- app.config.js
+++ app.config.js
@@ -8,7 +8,7 @@ export default {
     platforms: ['ios', 'android'],
     version: '0.15.0',
     orientation: 'portrait',
-    icon: './assets/flame.png',
+    icon: './assets/food.jpg',
     splash: {
       image: './assets/splash.png',
       resizeMode: 'cover',
